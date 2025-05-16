4755e8fabf8dd0bb4cebba007d67ba464779e982
Phillip Haberman <pjhab2020@gmail.com>
2025-05-16 14:57:41 +0800

    change layout to wide and markdown on about me page

--- about_me.py
+++ about_me.py
@@ -2,16 +2,15 @@ import streamlit as st
 
 # About Me content
 about_me = """
-<div style='font-size:18px; line-height:1.6;'>
+<div style='font-size:20px; line-height:1.5;'>
 Hi, I’m <strong>Phillip Haberman</strong> — a data analyst with experience turning messy data into meaningful insights.
-I specialize in <strong>Python</strong>, <strong>SQL</strong>, and <strong>BI tools</strong>, with a strong track record in the <strong>hospitality and restaurant industry</strong>.<br><br>
+I specialize in <strong>Python</strong>, <strong>SQL</strong>, and <strong>BI tools</strong>,
+with a strong track record in the <strong>hospitality and restaurant industry</strong>.
 
 Whether it’s uncovering trends, building dashboards, or improving operations, I love using data to solve real-world problems.
-<hr style='margin: 1em 0'>
 
 Outside of work, I enjoy reading, playing softball, and spending time with my wife and daughter.
 We love being outdoors, discovering new foods, and exploring new places together.
-<hr style='margin: 1em 0'>
 
 This site is where I share projects, ideas, and experiments as I grow my skills and explore new challenges in analytics.
 </div>
